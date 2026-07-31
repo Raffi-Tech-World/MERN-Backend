824ca175deaddaab891f6297914a348c201ff042
M.RAFFINA BANU <raffina411005@gmail.com>
2026-07-31 10:01:17 +0530

    Add files via upload

---
+++ Models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+
+const userSchema = new mongoose.Schema({
+    name: String,
+      email: String,
+      mobile: String,
+      password: String,
+      confirmPassword: String,
+
+})
+module.exports=mongoose.model("User",userSchema)
